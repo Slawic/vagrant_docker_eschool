c37428dc47323d2780d6bfde76ad3c8722f0b241
Serhikozak <47950830+Serhikozak@users.noreply.github.com>
2019-03-10 17:12:37 +0200

    Delete ScenarioDB.sh

--- ScenarioDB.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-  sudo yum -y install mariadb-server
-  sudo systemctl start mariadb
-  sudo systemctl enable mariadb
-
-
-  #firewall-cmd --add-port=3306/tcp 
-  #firewall-cmd --permanent --add-port=3306/tcp
