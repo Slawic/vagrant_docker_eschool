799905edf24c711ddf05a610b82663b9eeff5854
Serhikozak <47950830+Serhikozak@users.noreply.github.com>
2019-04-30 18:46:52 +0300

    Add files via upload

--- Vagrantfile
+++ Vagrantfile
@@ -1,38 +1,41 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+BOX_IMAGE = "centos/7"
+RAM = "512"
+AM_RAM = "2024"
+AM_CPU = "2"
+NETWORK_WEB = "192.168.56.10"
+NETWORK = "192.168.56.5"
+
 
 Vagrant.configure("2") do |config|
-  
-  config.vm.box = "centos/7"
 
-  
+    # config.vm.define "web" do |web|
+    #   web.vm.box = BOX_IMAGE
+    #   web.vm.hostname = "web"
+    #   web.vm.network :private_network, ip: NETWORK_WEB
+    #   web.vm.provider "virtualbox" do |vb|
+    #     vb.memory = RAM
+    #   end
+    # end
 
-  # config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
-
-  
-
-  # config.vm.network "public_network"
-  config.vm.network "private_network", ip: "192.168.56.110"
-
-  #SHA256:3lphOzzuOBvFxjJniJLcNH0C6+O76iI8/teH9noKlTo.
-
-  # config.vm.synced_folder "../data", "/vagrant_data"
-
-  
-
-  # config.vm.provider "virtualbox" do |vb|
-  #   # Display the VirtualBox GUI when booting the machine
-  #   vb.gui = true
-  #
-  #   # Customize the amount of memory on the VM:
-  #   vb.memory = "1024"
-  # end
-  #
-
-
-   config.vm.provision "shell", path: 'start.sh'
-  #   apt-get update
-  #   apt-get install -y apache2
-  # SHELL
+    config.vm.define "docker" do |docker|
+      docker.vm.box = BOX_IMAGE
+      docker.vm.hostname = "docker"
+      docker.vm.network :private_network, ip: NETWORK
+      docker.vm.provider "virtualbox" do |vb|
+        vb.memory = AM_RAM
+        vb.cpus = AM_CPU
+      end
+      #docker.vm.provision "file", source: ".vagrant/machines/web/virtualbox/private_key", destination: "~/.ssh/#{NETWORK_WEB}.pem"
+      docker.vm.provision "shell", path: "start.sh"
+               
+      docker.vm.provision "file", source: "vars.yml", destination: "~"  
+      docker.vm.provision "file", source: "maven.yml", destination: "~"     
+      docker.vm.provision "file", source: "jenkins.yml", destination: "~"      
+      docker.vm.provision "file", source: "jenkins_plugins.yml", destination: "~" 
+      docker.vm.provision "file", source: "main.yml", destination: "~"             
+        
+    end
 end
--- start.sh
+++ start.sh
@@ -1,46 +1,61 @@
-#!/bin/bash
-sudo yum -y update
-sudo yum -y install mc wget
-#install docker scenario from /docs.docker.com/install/linux
-sudo yum install -y yum-utils \
-  device-mapper-persistent-data \
-  lvm2
+#!/usr/bin/bash
 
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+# sudo yum -y update
+sudo yum -y install nano wget tree mc
 
-sudo yum -y install docker-ce docker-ce-cli containerd.io  
-sudo systemctl start docker 
-sudo systemctl enable docker  
-# create the docker group and add your user:
-sudo groupadd docker
-sudo usermod -aG docker $USER
-#how to restart machines?
-sudo systemctl restart docker 
+#Ansible
+sudo yum -y install epel-release
+sudo yum -y install ansible
 
-#coretto JDK JRE
-sudo wget https://d1f2yzg3dx5xke.cloudfront.net/java-1.8.0-amazon-corretto-devel-1.8.0_202.b08-1.amzn2.x86_64.rpm
-sudo wget https://d1f2yzg3dx5xke.cloudfront.net/java-1.8.0-amazon-corretto-1.8.0_202.b08-1.amzn2.x86_64.rpm
-sudo yum -y localinstall java-1.8.0-amazon-corretto*.rpm
-
-sudo mkdir /opt/maven
-
-    sudo wget https://apache.volia.net/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
-    sudo tar xzvf apache-maven-3.6.0-bin.tar.gz -C /opt/maven
-    sudo yum -y install maven
-    #setup the environment variables
-    #sudo sh -c "echo 'export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64/jre' >> /etc/profile.d/maven.sh"
-    sudo sh -c "echo 'export M2_HOME=/opt/maven/apache-maven-3.6.0' >> /etc/profile.d/maven.sh"
-    #sudo sh -c "echo 'export MAVEN_HOME=/opt/maven/apache-maven-3.6.0' >> /etc/profile.d/maven.sh"
-    sudo sh -c "echo 'PATH=${M2_HOME}/bin:${PATH}' >> /etc/profile.d/maven.sh"
-    #sudo chmod +x /etc/profile.d/maven.sh
-    #load the environment variables by typing
-    source /etc/profile.d/maven.sh
+sudo sed -i -e 's/#host_key_checking = False/host_key_checking = False/g' /etc/ansible/ansible.cfg
 
 
+cat <<EOF | sudo tee -a /etc/ansible/hosts
 
-#sudo docker build -t vagrant/8 .
+localhost  ansible_host=localhost ansible_user=vagrant  ansible_connection=local
+EOF
 
-#get the Nginx image
-#sudo docker pull nginx
+#sudo rm /etc/ansible/ansible.cfg
+#sudo chmod 400 ~/.ssh/*.pem
+
+# sudo yum-config-manager \
+#     --add-repo \
+#     https://download.docker.com/linux/centos/docker-ce.repo
+
+# sudo yum -y install docker-ce docker-ce-cli containerd.io  
+# sudo systemctl start docker 
+# sudo systemctl enable docker  
+# # create the docker group and add your user:
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# #how to restart machines?
+# sudo systemctl restart docker 
+
+# #coretto JDK JRE
+# sudo wget https://d1f2yzg3dx5xke.cloudfront.net/java-1.8.0-amazon-corretto-devel-1.8.0_202.b08-1.amzn2.x86_64.rpm
+# sudo wget https://d1f2yzg3dx5xke.cloudfront.net/java-1.8.0-amazon-corretto-1.8.0_202.b08-1.amzn2.x86_64.rpm
+# sudo yum -y localinstall java-1.8.0-amazon-corretto*.rpm
+
+# sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+# sudo yum -y install jenkins
+# sudo systemctl start jenkins.service
+# sudo systemctl enable jenkins.service
+
+
+
+# sudo mkdir /opt/maven
+
+#     sudo wget https://apache.volia.net/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
+#     sudo tar xzvf apache-maven-3.6.0-bin.tar.gz -C /opt/maven
+#     sudo yum -y install maven
+#     #setup the environment variables
+#     sudo sh -c "echo 'export JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64/jre' >> /etc/profile.d/maven.sh"
+#     sudo sh -c "echo 'export M2_HOME=/opt/maven/apache-maven-3.6.0' >> /etc/profile.d/maven.sh"
+#     sudo sh -c "echo 'export MAVEN_HOME=/opt/maven/apache-maven-3.6.0' >> /etc/profile.d/maven.sh"
+#     sudo sh -c "echo 'PATH=${M2_HOME}/bin:${PATH}' >> /etc/profile.d/maven.sh"
+#     sudo chmod +x /etc/profile.d/maven.sh
+#     #load the environment variables by typing
+#     source /etc/profile.d/maven.sh
+
+# sudo cat /var/lib/jenkins/secrets/initialAdminPassword > ~pass
